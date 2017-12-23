0c27a79ad6f6f3a6d96b7b275774cf84ccf2a64f
Christian Paul <info@jaller.de>
2017-12-23 00:17:51 -0800

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2,
+            {
+              "SwitchCase": 1
+            }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-console": 0,
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
