c155f511e2b912a770ffb130d8e5513aca99719f
ionmeo <rd528@cornell.edu>
2026-04-11 15:04:22 -0400

    added Windows support for npm global install

--- bin/mapscii.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-':' //; # Based on https://github.com/MrRio/vtop/blob/master/bin/vtop.js
-':' //; export TERM=xterm-256color
-':' //; exec "$(command -v node || command -v nodejs)" "$0" "$@"
-'use strict'
-require('../main.js');
